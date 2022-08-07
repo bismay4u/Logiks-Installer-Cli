c20bad58e1c9028bb882e17c80b45e12390d24b2
Bismay Kumar Mohapatra <bismay4u@gmail.com>
2022-08-07 22:25:20 +0530

    Update git-install.sh

--- git/git-install.sh
+++ git/git-install.sh
@@ -9,22 +9,27 @@ git clone --branch=master https://github.com/LogiksApps/Logiks-CMS.git cms/
 git clone --branch=master https://github.com/LogiksApps/_website.git home/
 
 cd ../plugins/modules/
+git clone --branch=master https://github.com/LogiksPlugins/charts.git
 git clone --branch=master https://github.com/LogiksPlugins/content.git
 git clone --branch=master https://github.com/LogiksPlugins/dashboard.git
 git clone --branch=master https://github.com/LogiksPlugins/datagrid.git
+git clone --branch=master https://github.com/LogiksPlugins/dcpages.git
 git clone --branch=master https://github.com/LogiksPlugins/forms.git
 git clone --branch=master https://github.com/LogiksPlugins/gallery.git
 git clone --branch=master https://github.com/LogiksPlugins/infoview.git
 git clone --branch=master https://github.com/LogiksPlugins/infoviewTable.git
+git clone --branch=master https://github.com/LogiksPlugins/infovisuals.git
 git clone --branch=master https://github.com/LogiksPlugins/navigator.git
+git clone --branch=master https://github.com/LogiksPlugins/packages.git
 git clone --branch=master https://github.com/LogiksPlugins/pages.git
 git clone --branch=master https://github.com/LogiksPlugins/reports.git
+git clone --branch=master https://github.com/LogiksPlugins/views.git
 
 cd ../../../
 pwd
 
-find -type d -name .git -exec rm -rf {} \;
-find -type d -name sql -exec rm -rf {} \;
+#find -type d -name .git -exec rm -rf {} \;
+#find -type d -name sql -exec rm -rf {} \;
 
 chmod -R 0777 logiks/tmp/
 
